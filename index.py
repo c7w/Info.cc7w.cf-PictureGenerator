1fc303ce31580ebcb6f972314f5d2b2dc9873089
c7w <admin@cc7w.cf>
2019-08-28 23:13:34 +0800

    Demo1

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
-from flask import Flask, Response, escape,request
+from flask import Flask, Response, escape,request,render_template
+from requests_html import HTMLSession
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def helloa():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
     
@@ -16,5 +18,24 @@ def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
     
+@app.route('/get/<path>')
+def getPath(path):
+    session = HTMLSession()
+    print(path)
+    try:
+        get = session.get('http://www.baidu.com')
+    except:
+        return "Nothing has been got."
+    return get.html.text
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    list = {
+        1: name,
+        2: 2333
+            }
+    return render_template('index.html' , list = list)
+
 if __name__ == '__main__':
     app.run()
