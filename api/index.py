9cdbe995c23fb2d17e8be2833f90343117cb63aa
c7w <admin@cc7w.cf>
2019-08-28 21:45:59 +0800

    适配.7

--- api/index.py
+++ api/index.py
@@ -2,6 +2,15 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
 def catch_all(path):
     return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+    
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
