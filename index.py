90818a7eb25be42e0d6a8b26f061e88767afe4c8
c7w <admin@cc7w.cf>
2019-08-28 22:42:46 +0800

    t.3

--- index.py
+++ index.py
@@ -6,14 +6,19 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 api = Api(app,
           version='0.1',
-          title='Our sample API',
-          description='This is our sample API'
+          title='c7w写的破程序',
+          description='c7w写的破API'
 )
 
 @api.route('/hello_world')
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+@api.route('/')
+class Hello(Resource):
+    def get(self):
+        return "this"
 
 if __name__ == '__main__':
     app.run(debug=True)
