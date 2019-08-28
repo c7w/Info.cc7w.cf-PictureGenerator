da7e79efa84ad3040eafeb6d4778f1577519ecb0
c7w <admin@cc7w.cf>
2019-08-28 21:25:49 +0800

    适配.5

--- api/views.py
+++ api/views.py
@@ -3,16 +3,6 @@ from requests_html import HTMLSession
 import re
 
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
-
-class handler(BaseHTTPRequestHandler):
-
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    return
 
 # Create your views here.
 def index(request):
