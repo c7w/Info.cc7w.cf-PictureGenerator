d01f124187fd11b0c72490b3ba3518bf2501c27d
c7w <admin@cc7w.cf>
2019-10-26 20:06:06 +0800

    重构代码

--- index.py
+++ index.py
@@ -1,6 +1,4 @@
-import utils.getMcbbsScore as score
 import utils.pic as pic
-
 
 from PIL import Image
 from datetime import datetime,timedelta
@@ -14,46 +12,49 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-##################################################
+###########################################################
 # 图片: map
 # 描述: 进度条
-# 参数: 
-#####初始值: start
-#####当前值: now
-#####目标值: end
-#####进度条颜色: colorBar  默认值 None  示例输入(0,0,0,255)
-#####完成进度条颜色: colorComplete  默认值 (0,0,0,255)  示例输入(0,0,0,255)
-#####进度条介绍: description  默认值 ('Unknown','A',20)  示例输入('介绍','A',12)
-#####初始值介绍: startMessage  默认值 ('','A',20)
-#####当前值介绍: nowMessage  默认值 ('','A',20)
-#####目标值介绍: endMessage  默认值 ('','A',20)
-#####是否展示百分比: showPercentage  默认值(True,1,'A',20)
 # 起始点 (50,50) 终止点 (450,80)
 # 进度条颜色 (255,255,255) 进度条边框颜色 (21,100,43)
+###########################################################
+# 参数: 
+#[i] - 初始值
+#<v> - 当前值
+#<f> - 最终值
+#[mode]:
+#   [normal] - 正常模式
+#   time - 时间间隔 输入时间戳
+#   bbs - BBS积分
+#[c] - 进度条颜色（空） [(235,205,226,255)]
+#[cc] - 进度条颜色（填满） [(143,240,152,255)]
+#[font] - 字体 (字体标号,hm字体大小,信息字体大小,百分比大小) [('A',20,18,20)]
+#[im] - 初始值信息
+#[vm] - 当前值信息
+#[fm] - 最终值信息
+#[hm] - 标题 ['Unknown']
+#[percent] - 是否展示百分比 [(True,2)]
+###########################################################
 @app.route('/map')
 def map():
     def getParam(param,default):
-        return request.args.get(param) or default
-    start = float(getParam('start',0))
-    now = float(getParam('now',50))
-    end = float(getParam('end',100))
-    colorBar = tuple(eval(getParam('colorBar','(235,205,226,255)')))
-    colorComplete = tuple(eval(getParam('colorComplete','(143,240,152,255)')))
-    startMessage = tuple(eval(getParam('startMessage','("","A",20)')))
-    nowMessage = tuple(eval(getParam('nowMessage','("","A",20)')))
-    endMessage = tuple(eval(getParam('endMessage','("","A",20)')))
-    description = tuple(eval(getParam('description','("Unknown","A",20)')))
-    showPercentage = tuple(eval(getParam('showPercentage','(True,1,"A",20)')))
-
-    if now == 564032:
-        now = int(score.getScoreFromUid(564032).get('score')[0])
-        startM = getParam('s','')
-        endM = getParam('e','')
-        nowM = "当前积分: "+str(now)
-        startMessage = (startM,startMessage[1],startMessage[2])
-        nowMessage = (nowM,nowMessage[1],nowMessage[2])
-        endMessage = (endM,endMessage[1],endMessage[2])
-    return pic.returnImage(pic.map(start,now,end,colorBar=colorBar,colorComplete=colorComplete,startMessage=startMessage,nowMessage=nowMessage,endMessage=endMessage,description=description,showPercentage=showPercentage))
+        return request.args.get(param,default)
+    try:
+        i = float(request.args.get('i'))
+        v = float(request.args.get('v'))
+        f = float(request.args.get('f'))
+    except:
+        return index()
+    mode = getParam('mode','normal')
+    c = tuple(eval(getParam('c','(235,205,226,255)')))
+    cc = tuple(eval(getParam('cc','(143,240,152,255)')))
+    font = tuple(eval(getParam('font','("A",20,18,20)')))
+    im = getParam('im','')
+    vm = getParam('vm','')
+    fm = getParam('fm','')
+    hm = getParam('hm','Unknown')
+    percent = tuple(eval(getParam('percent','(True,2)')))
+    return pic.returnImage(pic.map(i,v,f,mode,c,cc,font,im,vm,fm,hm,percent))
     
 if __name__ == '__main__':
     app.run()
--- utils/pic.py
+++ utils/pic.py
@@ -1,7 +1,18 @@
+import utils.getMcbbsScore as score
+
+import time
 import base64
 from io import BytesIO
 from flask import Flask, Response,escape,request,render_template,send_file
 from PIL import Image,ImageFont,ImageDraw
+
+def loadImage(imgName):
+    f = open('./templates/assets/'+imgName+'.txt')
+    byte_data = base64.b64decode(f.read().rstrip('\n'))
+    f.close()
+    image_data = BytesIO(byte_data)
+    img = Image.open(image_data)
+    return img
 
 def returnImage(img):
     img_io = BytesIO()
@@ -10,8 +21,12 @@ def returnImage(img):
     return send_file(img_io, mimetype='image/png', cache_timeout=0)
 
 def getFont(font,size):
-    font = ImageFont.truetype('./templates/font/font'+font+'.ttf',size)
-    return font
+    try:
+        font = ImageFont.truetype('./templates/font/font'+font+'.ttf',size)
+        return font
+    except:
+        font = ImageFont.truetype('./templates/font/fontA.ttf',size)
+        return font
 
 def writeText(img,text,pos,align='left',font='A',size=12,color=(0,0,0)):
     if text == '':
@@ -28,70 +43,79 @@ def writeText(img,text,pos,align='left',font='A',size=12,color=(0,0,0)):
     imgDraw = ImageDraw.Draw(img)
     imgDraw.text(pos2,text,fill=color,font=font)
 
-##################################################
+###########################################################
 # 图片: map
 # 描述: 进度条
-# 参数: 
-#####初始值: start
-#####当前值: now
-#####目标值: end
-#####进度条颜色: colorBar  默认值 None  示例输入(0,0,0,255)
-#####完成进度条颜色: colorComplete  默认值 (0,0,0,255)  示例输入(0,0,0,255)
-#####进度条介绍: description  默认值 ('Unknown','A',20)  示例输入('介绍','A',12)
-#####初始值介绍: startMessage  默认值 ('','A',20)
-#####当前值介绍: nowMessage  默认值 ('','A',20)
-#####目标值介绍: endMessage  默认值 ('','A',20)
-#####是否展示百分比: showPercentage  默认值(True,1,'A',20)
 # 起始点 (50,50) 终止点 (450,80)
 # 进度条颜色 (255,255,255) 进度条边框颜色 (21,100,43)
-def map(start,now,end,colorBar=None,colorComplete=(0,0,0,255),description=('Unknown','A',20),startMessage=('','A',20),nowMessage=('','A',20),endMessage=('','A',20),showPercentage=(True,1,'A',20)):
-    img = Image.open('./templates/assets/map.png')
+###########################################################
+# 参数: 
+#[i] - 初始值
+#<v> - 当前值
+#<f> - 最终值
+#[mode]:
+#   [normal] - 正常模式
+#   time - 时间间隔 输入时间戳
+#   bbs - BBS积分
+#[c] - 进度条颜色（空） [(235,205,226,255)]
+#[cc] - 进度条颜色（填满） [(143,240,152,255)]
+#[font] - 字体 (字体标号,hm字体大小,信息字体大小,百分比大小) [('A',20,18,15)]
+#[im] - 初始值信息
+#[vm] - 当前值信息
+#[fm] - 最终值信息
+#[hm] - 标题 ['Unknown']
+#[percent] - 是否展示百分比/保留小数位数 [(True,2)]
+###########################################################
+def map(i,v,f,mode,c,cc,font,im,vm,fm,hm,percent):
+    img = loadImage('map')
     barWidth = 400
     barHeight = 30
     
-    if colorBar:
-        for x in range(50,51+barWidth):
-            for y in range(50,51+barHeight):
-                currentPixel = img.getpixel((x,y))
-                if (currentPixel[2] >=180):
-                    img.putpixel((x,y),colorBar)
+    #处理模式
+    if mode == 'bbs':
+        v = int(score.getScoreFromUid(v).get('score')[0])
+        vm = "当前积分: "+str(v)
+        
+    if mode == 'time':
+        v = time.time()
 
+    #绘制空进度条
+    for x in range(50,51+barWidth):
+        for y in range(50,51+barHeight):
+            currentPixel = img.getpixel((x,y))
+            if (currentPixel[2] >=180):
+                img.putpixel((x,y),c)
 
     #percentage 百分比
     #pixel 应绘制的像素值
-    delta = now - start
-    duration = end - start
-    percentage = delta / duration
-    
-    if percentage>1:
-        percentage=1
-    
+    try:
+        delta = v - i
+        duration = f - i
+        percentage = delta / duration
+        if percentage > 1:
+            percentage = 1
+    except:
+            percentage = 1
+            cc = (0,0,0,255)
+            hm = '！！！计算错误！！！'
+        
     pixelDelta = int(percentage * 396)
-    
     pixelFrom = 50
     pixelTo = 51 + pixelDelta
     
-    #绘制进度条
-    if colorBar:
-        for x in range(50,pixelTo):
-            for y in range(50,51+barHeight):
-                currentPixel = img.getpixel((x,y))
-                if (currentPixel == colorBar):
-                    img.putpixel((x,y),colorComplete)
-    else:
-        for x in range(50,pixelTo):
-            for y in range(50,51+barHeight):
-                currentPixel = img.getpixel((x,y))
-                if (currentPixel[2] >=180):    
-                    img.putpixel((x,y),colorComplete)
+    #填充进度条
+    for x in range(50,pixelTo):
+        for y in range(50,51+barHeight):
+            currentPixel = img.getpixel((x,y))
+            if (currentPixel == c):
+                img.putpixel((x,y),cc)
     
     #添加文字
-    writeText(img,description[0],(250,20),align='center',font=description[1],size=description[2])
-    writeText(img,startMessage[0],(50,90),align='left',font=startMessage[1],size=startMessage[2])
-    writeText(img,nowMessage[0],(250,90),align='center',font=nowMessage[1],size=nowMessage[2])
-    writeText(img,endMessage[0],(450,90),align='right',font=endMessage[1],size=endMessage[2])
-    if showPercentage[0]:
-        percentageMessage = str(round(percentage*100,2)) + '%'
-    writeText(img,percentageMessage,(250,55),align='center',font=showPercentage[2],size=showPercentage[3])
-
+    writeText(img,hm,(250,20),align='center',font=font[0],size=font[1])
+    writeText(img,im,(50,90),align='left',font=font[0],size=font[2])
+    writeText(img,vm,(250,90),align='center',font=font[0],size=font[2])
+    writeText(img,fm,(450,90),align='right',font=font[0],size=font[2])
+    if percent[0]:
+        pm = str(round(percentage*100,percent[1])) + '%'
+    writeText(img,pm,(250,55),align='center',font=font[0],size=font[3])
     return img
